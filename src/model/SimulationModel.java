a301371ef382165edbb67092d518519d019835c9
js2814 <js2814@g.rit.edu>
2024-12-05 16:43:50 -0500

    Completed basic initialization functionality

--- src/model/SimulationModel.java
+++ src/model/SimulationModel.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -8,13 +9,90 @@ import java.util.List;
  * @author Justin Shen - js2814@rit.edu
  */
 public class SimulationModel {
-    private List<Observer<SimulationModel>> observers;
+    private List<Observer<SimulationModel>> observers; // observers of the model
+    private int populationSize; // current size of the population
+    private int generation; // current generation (initial = 0)
+    private int males; // number of males in the population
+    private int females; // number of females in the population
+    private int colorblindMales;
+    private int colorblindFemales;
+    private int carrierFemales;
 
+    /**
+     * Constructor, initializes observers list and other necessary variables
+     */
     public SimulationModel(){
-
+        this.observers = new LinkedList<>();
+        this.generation = 0;
     }
 
+    /**
+     * Add an observer to the model
+     * @param observer The observer to add (PTUI/GUI)
+     */
     public void addObserver(Observer<SimulationModel> observer){
         this.observers.add(observer);
+    }
+
+    /**
+     * Notify the observers when the model changes
+     */
+    private void notifyObservers() {
+        for (Observer<SimulationModel> observer: this.observers ) {
+            observer.update(this);
+        }
+    }
+
+    /**
+     * Set population size
+     * @param populationSize The starting population size (>= 30)
+     */
+    public void setPopulationSize(int populationSize){
+        this.populationSize = populationSize;
+    }
+
+    /**
+     * Set male/female populations based on percentages
+     */
+    public void setPercentages(double malePercentage){
+        this.males = (int) (this.populationSize * (malePercentage / 100.0));
+        this.females = this.populationSize - this.males;
+    }
+
+    /**
+     * Set colorblindness/carrier status based on percentages
+     * @param maleColorBlindPercentage percentage of males that are colorblind
+     * @param femaleColorBlindPercentage percentage of females that are colorblind
+     * @param femaleCarrierPercentage percentage of females that are carriers
+     */
+    public void setColorblind(double maleColorBlindPercentage, double femaleColorBlindPercentage,
+                              double femaleCarrierPercentage){
+        this.colorblindMales = (int) (this.males * (maleColorBlindPercentage / 100.0));
+        this.colorblindFemales = (int) (this.females * (femaleColorBlindPercentage / 100.0));
+        this.carrierFemales = (int) ( this.females * (femaleCarrierPercentage / 100.0));
+    }
+
+    /**
+     * toString of the model for the PTUI, prints basic data
+     */
+    @Override
+    public String toString(){
+        String result = "";
+        result += "Generation " + this.generation; // Generation number
+        result += "\nPopulation Size: " + this.populationSize; // Population size
+        result += "\nMales: " + this.males + " (" + String.format("%.2f", (double) this.males
+                / this.populationSize * 100.0) + "% of total population)"; // Male %
+        result += "\nFemales: " + this.females + " (" + String.format("%.2f", (double) this.females
+                / this.populationSize * 100.0) + "% of total population)"; // Female %
+        result += "\nColorblindness: " + (this.colorblindMales + this.colorblindFemales) + " individuals"
+                + " are colorblind (" + String.format("%.2f", (double) (this.colorblindMales +
+                this.colorblindFemales) / this.populationSize * 100) + "% of total population)"; // Colorblind %
+        result += "\n" + this.colorblindMales + " males are colorblind (" + String.format("%.2f", (double)
+                this.colorblindMales / this.males * 100) + "% of total males)"; // Colorblind Males
+        result += "\n" + this.colorblindFemales + " females are colorblind (" + String.format("%.2f", (double)
+                this.colorblindFemales / this.females * 100) + "% of total females)"; // Colorblind Females
+        result += "\n" + this.carrierFemales + " females are carriers (" + String.format("%.2f", (double)
+                this.carrierFemales / this.females * 100) + "% of total females)"; // Colorblind Females
+        return result;
     }
 }
--- src/ptui/SimulationPTUI.java
+++ src/ptui/SimulationPTUI.java
@@ -11,18 +11,29 @@ import java.util.Scanner;
  */
 public class SimulationPTUI implements Observer<SimulationModel> {
     private SimulationModel model;
+    /** Real-life statistics are used for the default values */
+    private static final double DEFAULT_MALE_COLORBLIND = 8.0;
+    private static final double DEFAULT_FEMALE_COLORBLIND = 0.5;
+    private static final double DEFAULT_FEMALE_CARRIER = 15.0;
 
+    /**
+     * Constructor, initializes the model and adds itself as an observer
+     */
     public SimulationPTUI(){
         this.model = new SimulationModel();
         this.model.addObserver(this);
     }
 
     public void update(SimulationModel model){
-
+        System.out.println(this.model);
     }
 
+    /**
+     * Main loop for functionality, prompts user for input and runs the simulation based on it
+     */
     public void run(){
         Scanner in = new Scanner(System.in);
+        // Get population size from input then update model (must be >= 30)
         int populationSize = 0;
         while (populationSize < 30){
             System.out.print("Enter population size: ");
@@ -32,8 +43,62 @@ public class SimulationPTUI implements Observer<SimulationModel> {
             }
         }
         System.out.println("Total population size set to " + populationSize);
+        this.model.setPopulationSize(populationSize);
+        // Get male percentage from input then update model (must be between 0 and 100)
+        double malePercentage = 0.0;
+        while (malePercentage <= 0.0 || malePercentage >= 100.0){
+            System.out.print("Enter the percentage of males in the population: ");
+            malePercentage = in.nextDouble();
+            if (malePercentage <= 0.0 || malePercentage >= 100.0){
+                System.out.println("Percentage must be between 0.0 and 100.0");
+            }
+        }
+        System.out.println("Percentage of males set to: " + malePercentage + "%");
+        System.out.println("Percentage of females set to: " + (100 - malePercentage) + "%");
+        this.model.setPercentages(malePercentage);
+        // Get colorblindness/carrier percentages from input then update model
+        double maleColorBlindPercentage = 0.0;
+        double femaleColorBlindPercentage = 0.0;
+        double femaleCarrierPercentage = 0.0;
+        while (maleColorBlindPercentage <= 0.0 || maleColorBlindPercentage >= 100.0){
+            System.out.print("Enter the percentage of males that are colorblind (-1 to use defaults): ");
+            maleColorBlindPercentage = in.nextDouble();
+            if (maleColorBlindPercentage == -1){
+                maleColorBlindPercentage = DEFAULT_MALE_COLORBLIND;
+            } else if (maleColorBlindPercentage <= 0.0 || maleColorBlindPercentage >= 100.0){
+                System.out.println("Percentage must be between 0.0 and 100.0");
+            }
+        }
+        while (femaleColorBlindPercentage <= 0.0 || femaleColorBlindPercentage >= 100.0){
+            System.out.print("Enter the percentage of females that are colorblind (-1 to use defaults): ");
+            femaleColorBlindPercentage = in.nextDouble();
+            if (femaleColorBlindPercentage == -1){
+                femaleColorBlindPercentage = DEFAULT_FEMALE_COLORBLIND;
+            } else if (femaleColorBlindPercentage <= 0.0 || femaleColorBlindPercentage >= 100.0){
+                System.out.println("Percentage must be between 0.0 and 100.0");
+            }
+        }
+        while (femaleCarrierPercentage <= 0.0 || femaleCarrierPercentage >= 100.0){
+            System.out.print("Enter the percentage of females that are carriers (-1 to use defaults): ");
+            femaleCarrierPercentage = in.nextDouble();
+            if (femaleCarrierPercentage == -1){
+                femaleCarrierPercentage = DEFAULT_FEMALE_CARRIER;
+            } else if (femaleCarrierPercentage <= 0.0 || femaleCarrierPercentage >= 100.0){
+                System.out.println("Percentage must be between 0.0 and 100.0");
+            }
+        }
+        System.out.println("Percentage of males with color blindness set to: " + maleColorBlindPercentage + "%");
+        System.out.println("Percentage of females with color blindness set to: " + femaleColorBlindPercentage + "%");
+        System.out.println("Percentage of female carriers of color blindness set to: " + femaleCarrierPercentage + "%");
+        model.setColorblind(maleColorBlindPercentage, femaleColorBlindPercentage, femaleCarrierPercentage);
+        // Generation 0
+        this.update(model);
     }
 
+    /**
+     * Main method, creates the PTUI and runs it
+     * @param args None
+     */
     public static void main(String[] args){
         SimulationPTUI ptui = new SimulationPTUI();
         ptui.run();
